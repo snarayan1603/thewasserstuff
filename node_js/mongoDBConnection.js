fb6e2ad5e9dc2793d997dd49d45c1bfe4885c121
snarayan1603 <120448155+snarayan1603@users.noreply.github.com>
2022-12-21 13:12:48 +0530

    Update mongoDBConnection.js

--- node_js/mongoDBConnection.js
+++ node_js/mongoDBConnection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 
-const uri = `mongodb+srv://snarayan1603:Bablu123@cluster0.c9iqpz4.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://xxxxxxxxxxxx:xxxxxxxx@cluster0.c9iqpz4.mongodb.net/test?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri);
 
